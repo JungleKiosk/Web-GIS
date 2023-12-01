190509d64776b938efe13c389f1e32cfe00cedb9
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-12-01 23:50:48 +0100

    create geoJSON & put on map 🗺️

---
+++ data/markers.js
@@ -0,0 +1,108 @@
+/* create JSON from:
+https://geojson.io/#map=12.64/37.92235/12.31038
+copy & paste in markers.js
+*/
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            11.809118657745898,
+            45.44790850907958
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            11.381047310923634,
+            45.21158402685384
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            10.942471145775244,
+            45.501313771149995
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            12.340996130039002,
+            37.93602863353074
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            12.32288126777783,
+            37.930398120648206
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            12.314298874347003,
+            37.94327266289092
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            12.286401604762204,
+            37.94251332627381
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            12.27222295528361,
+            37.93519356663519
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            12.277926105762731,
+            37.933227189881705
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
